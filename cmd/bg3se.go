db93c0919bb67637ba4af1c749bc026d0a08b63f
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-01-19 12:01:08 +0100

    add status command for bg3se

--- cmd/bg3se.go
+++ cmd/bg3se.go
@@ -5,9 +5,12 @@ import (
 	"github.com/GiGurra/boa/pkg/boa"
 	"github.com/GiGurra/bookeeper/pkg/config"
 	"github.com/GiGurra/bookeeper/pkg/github"
+	"github.com/GiGurra/bookeeper/pkg/gui_tree"
 	"github.com/GiGurra/bookeeper/pkg/modzip"
 	"github.com/spf13/cobra"
+	"github.com/xlab/treeprint"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -25,6 +28,7 @@ func Bg3SeCmd() *cobra.Command {
 		Params:      cfg,
 		ParamEnrich: boa.ParamEnricherDefault,
 		SubCommands: []*cobra.Command{
+			Bg3SeStatusCmd(),
 			Bg3SeInstallCmd(),
 		},
 	}.ToCmd()
@@ -72,6 +76,38 @@ func Bg3SeInstallCmd() *cobra.Command {
 			cmdStr := "%command%"
 			//goland:noinspection GoPrintFunctions
 			fmt.Println(" WINEDLLOVERRIDES=\"DWrite.dll=n,b\" " + cmdStr)
+		},
+	}.ToCmd()
+}
+
+func Bg3SeStatusCmd() *cobra.Command {
+
+	cfg := &Bg3SeCmdConfig{}
+
+	return boa.Wrap{
+		Use:         "status",
+		Short:       "show status of bg3se (BG3 Script Extender)",
+		Params:      cfg,
+		ParamEnrich: boa.ParamEnricherDefault,
+		Run: func(cmd *cobra.Command, args []string) {
+
+			bg3SeDllPath := config.Bg3SeDllPath(&cfg.Base)
+			bg3SeInstalled := config.ExistsFile(bg3SeDllPath)
+
+			rootNode := treeprint.New() // NewWithRoot("Bookeeper Status")
+
+			///////////// bg3 paths /////////////////////////////////////////
+			bg3PathsNode := gui_tree.AddChildStr(rootNode, "bg3 paths")
+
+			installNode := gui_tree.AddKV(bg3PathsNode, "bg3 install dir", config.Bg3Path(&cfg.Base))
+
+			bg3SeNode := gui_tree.AddChildStr(installNode, "bg3se status")
+			gui_tree.AddKV(bg3SeNode, "installed", strconv.FormatBool(bg3SeInstalled))
+			gui_tree.AddKV(bg3SeNode, "dll path", bg3SeDllPath)
+			gui_tree.MakeChildrenSameKeyLen(bg3SeNode)
+
+			/////////////////////////////////////////////////////////////////////
+			fmt.Println(rootNode.String())
 		},
 	}.ToCmd()
 }
