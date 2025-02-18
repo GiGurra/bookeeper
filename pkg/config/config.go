3246e62c8afd6ea8ee88a8a47dbd25f3ec3c5dca
Johan Kjölhede <johan.kjolhede@gmail.com>
2025-02-18 22:51:20 +0100

    try to support windows

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/GiGurra/boa/pkg/boa"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -13,14 +12,6 @@ import (
 const (
 	SteamBg3AppID = "1086940"
 )
-
-type BaseConfig struct {
-	Verbose   boa.Required[bool]   `name:"verbose" short-name:"v" default:"false"`
-	SteamPath boa.Required[string] `default:"${HOME}/.steam/steam" name:"steam-path" short-name:"s"`
-	//UserDataPath       boa.Required[string] `default:"${SteamPath}/userdata/[0]" name:"user-data-path" short-name:"u"`
-	ModsInstallDir     boa.Required[string] `default:"${SteamPath}/steamapps/compatdata/1086940/pfx/drive_c/users/steamuser/AppData/Local/Larian Studios/Baldur's Gate 3/Mods" name:"mods-install-dir" short-name:"m"`
-	ModSettingsLsxPath boa.Required[string] `default:"${SteamPath}/steamapps/compatdata/1086940/pfx/drive_c/users/steamuser/AppData/Local/Larian Studios/Baldur's Gate 3/PlayerProfiles/Public/modsettings.lsx" name:"mod-settings-lsx-path" short-name:"l"`
-}
 
 func HomeDir() string {
 	res, err := os.UserHomeDir()
